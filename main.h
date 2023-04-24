a2c836c13f8ab34d9e1ad8e0650f6c6e9e42bfa4
Sahar <Aguidi53@gmail.com>
2023-04-24 09:34:14 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#define MAIN_H
+#ifndef MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
+int	 _putchar(char c);
+int	 _printf(const char *format, ...);
+
+
+#endif
