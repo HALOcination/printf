d9b61c184b7f6c59671c36a37c8310502cad27aa
Sahar <Aguidi53@gmail.com>
2023-04-24 12:33:47 -0700

    main.h

--- main.h
+++ main.h
@@ -12,22 +12,22 @@
 
 int _putchar(char c);
 int buffer(char c);
-int _printf(const char format, ...);
+int _printf(const char *format, ...);
 
 typedef struct format_t
 {
         char t;
-        int (f)(va_list);
+        int (*f)(va_list);
 } format;
 
 int _print_string(va_list list);
 int _print_char(va_list list);
 int _print_int(va_list list);
-int _strlen(const char s);
-int print(chars);
+int _strlen(const char *s);
+int print(char *s);
 int _put_string(char str);
-int checker(const chars, va_list list);
-int check(const char s, va_list list, inti);
+int checker(const char *s, va_list list);
+int check(const char *s, va_list list, int *i);
 int _print_int(va_list list);
 char *_ita(long int n, int base);
 
